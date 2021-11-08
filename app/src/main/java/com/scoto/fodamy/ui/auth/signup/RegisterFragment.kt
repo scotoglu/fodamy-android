3c4b39f201c16383bea9c927775a779b1ee890ad
Scoto <scotoglu@gmail.com>
2021-11-08 14:31:45 +0300

    Design register screen

--- app/src/main/java/com/scoto/fodamy/ui/auth/signup/RegisterFragment.kt
+++ app/src/main/java/com/scoto/fodamy/ui/auth/signup/RegisterFragment.kt
@@ -1,18 +1,29 @@
 package com.scoto.fodamy.ui.auth.signup
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.scoto.fodamy.R
 import com.scoto.fodamy.databinding.FragmentRegisterBinding
+import com.scoto.fodamy.ext.spannableText
 import com.scoto.fodamy.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegisterFragment :
     BaseFragment<FragmentRegisterBinding, RegisterViewModel>(R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.tvHaveAccount.text =
+            getString(R.string.login).spannableText(getString(R.string.have_account))
+
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
 
     override fun getViewModelClass(): Class<RegisterViewModel> = RegisterViewModel::class.java
 }
