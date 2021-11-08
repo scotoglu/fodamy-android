23091c2831ba32ec249146392d9a40bf8fb76f37
Scoto <scotoglu@gmail.com>
2021-11-08 14:16:26 +0300

    Design login screen and add dimension file.

--- app/src/main/java/com/scoto/fodamy/ext/StringExt.kt
+++ app/src/main/java/com/scoto/fodamy/ext/StringExt.kt
@@ -1,1 +1,42 @@
 package com.scoto.fodamy.ext
+
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+
+fun String.spannableText(text: String = ""): SpannableString {
+    val start = text.length
+    val end = if (text.isEmpty()) this.length else text.length + this.length
+
+    val spannable = SpannableString(text + this)
+    spannable.setSpan(
+        ForegroundColorSpan(Color.RED),
+        start,
+        end,
+        SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    return spannable
+
+}
+
+fun SpannableString.withClickable(
+    clickablePart: String,
+    onClickListener: () -> Unit
+): SpannableString {
+    val clickableSpan = object : ClickableSpan() {
+        override fun onClick(widget: View) {
+            onClickListener.invoke()
+        }
+    }
+    val clickablePartStart = indexOf(clickablePart)
+    setSpan(
+        clickableSpan,
+        clickablePartStart,
+        clickablePartStart + clickablePart.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    return this
+}
--- app/src/main/java/com/scoto/fodamy/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/scoto/fodamy/ui/auth/login/LoginFragment.kt
@@ -1,7 +1,11 @@
 package com.scoto.fodamy.ui.auth.login
 
+import android.os.Bundle
+import android.view.View
 import com.scoto.fodamy.R
 import com.scoto.fodamy.databinding.FragmentLoginBinding
+import com.scoto.fodamy.ext.spannableText
+import com.scoto.fodamy.ext.withClickable
 import com.scoto.fodamy.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,5 +13,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(
     R.layout.fragment_login
 ) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDontHaveAccount.text =
+            getString(R.string.signup).spannableText(getString(R.string.dont_have_account))
+
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+    }
+
+
     override fun getViewModelClass(): Class<LoginViewModel> = LoginViewModel::class.java
 }
