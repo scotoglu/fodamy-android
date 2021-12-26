c8e01b4d3e054f9b7afc294084384b7658535614
Scoto <scotoglu@gmail.com>
2021-12-26 16:28:02 +0300

    Remove binding adapter from each package

--- app/src/main/java/com/scoto/fodamy/util/BindingAdapters.kt
+++ app/src/main/java/com/scoto/fodamy/util/BindingAdapters.kt
@@ -1,11 +1,14 @@
 package com.scoto.fodamy.ui.home.adapter
 
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.scoto.fodamy.R
 import com.scoto.fodamy.ext.loadCircleImageFromUrl
 import com.scoto.fodamy.ext.loadImageFromUrl
+import com.scoto.fodamy.ext.spannableNum
 import com.scoto.fodamy.util.CustomToolbar
 
 @BindingAdapter(value = ["app:recipeCount", "app:followerCount"])
@@ -48,4 +51,41 @@ fun setOnBackListener(toolbar: CustomToolbar, listener: CustomToolbar.OnBackList
 @BindingAdapter("app:onEndIconListener")
 fun setOnEndIconListener(toolbar: CustomToolbar, listener: CustomToolbar.OnEndIconListener) {
     toolbar.onEndIconListener = listener
+}
+@BindingAdapter(value = ["comments", "likes"])
+fun setCommentAndLikeCount(tv: TextView, comments: Int?, likes: Int?) {
+    tv.text = tv.context.resources.getString(
+        R.string.comment_and_like_count,
+        comments ?: 0,
+        likes ?: 0
+    )
+}
+
+@BindingAdapter("app:comments")
+fun setCommentsCount(tv: TextView, comments: Int?) {
+    val commentTxt = tv.context.resources.getString(R.string.comment, comments)
+    tv.text = commentTxt.spannableNum(0, comments.toString().length)
+}
+@BindingAdapter("app:likes")
+fun setLikes(tv: TextView, likes: Int?) {
+    likes?.let {
+        val likesTxt = tv.context.resources.getString(R.string.like, likes)
+        tv.text = likesTxt.spannableNum(0, likes.toString().length)
+    }
+}
+
+@BindingAdapter("app:emptyComment")
+fun setVisibilityEmptyComment(view: View, visibility: Boolean) {
+    view.isVisible = visibility
+}
+
+@BindingAdapter("app:timeOfRecipe")
+fun setTimeOfRecipe(tv: TextView, timeOfRecipe: String?) {
+    timeOfRecipe?.let {
+        tv.text = tv.context.resources.getString(R.string.timeOfRecie, timeOfRecipe)
+    }
+}
+@BindingAdapter("progressbarState")
+fun setProgressbarState(view: View, state: Boolean) {
+    view.isVisible = state
 }
