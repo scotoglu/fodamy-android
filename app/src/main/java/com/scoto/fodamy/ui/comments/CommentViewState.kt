ec8eb9cf9c8983cad8a31bf58ba4353b176f6361
Scoto <scotoglu@gmail.com>
2021-12-30 00:32:22 +0300

    Move edit comment from fragment to viewmodel

--- app/src/main/java/com/scoto/fodamy/ui/comments/CommentViewState.kt
+++ app/src/main/java/com/scoto/fodamy/ui/comments/CommentViewState.kt
@@ -3,5 +3,4 @@ package com.scoto.fodamy.ui.comments
 sealed class CommentViewState {
     data class Success(val message: String) : CommentViewState()
     data class CommentEdited(val message: String) : CommentViewState()
-    object EditModeAndBack : CommentViewState()
 }
