81dfb01f29a382d3c90441bb0bc0b50b2273e6c7
Scoto <scotoglu@gmail.com>
2022-03-31 10:45:28 +0300

    Add swipe refresh layout

--- data/src/main/java/com/scoto/data/local/dao/RecipeDao.kt
+++ data/src/main/java/com/scoto/data/local/dao/RecipeDao.kt
@@ -2,7 +2,6 @@ package com.scoto.data.local.dao
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
