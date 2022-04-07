51aee58cac96746a242ade593350c2ccb8ee2f94
Scoto <scotoglu@gmail.com>
2022-04-07 10:01:31 +0300

    ...

--- app/src/main/java/com/scoto/fodamy/util/Constants.kt
+++ app/src/main/java/com/scoto/fodamy/util/Constants.kt
@@ -1,15 +1,11 @@
 package com.scoto.fodamy.util
 
-// Retrofit Constants
-const val BASE_URL = "https://fodamy.mobillium.com/api/"
-
 // Methods will be called by below types
 const val FROM_EDITOR_CHOICE = "FROM_EDITOR_CHOICE"
 const val FROM_LAST_ADDED = "FROM_LAST_ADDED"
 const val FROM_RECIPES_BY_CATEGORY = "FROM_RECIPES_BY_CATEGORY"
 
 // Dialog Constants
-
 const val DIALOG_ACTION = "DialogAction"
 const val KEY_UNFOLLOW_COMPLETED = "unfollow_completed"
 const val KEY_COMMENT_EDIT = "comment_edit_mode"
